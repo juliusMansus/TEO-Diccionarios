3fa394926d712c9fd9f4f32d8f9e69a7b4142e30
juliusMansus <caballerojgil@gmail.com>
2024-01-11 20:05:08 +0100

    new file:   src/__pycache__/bso.cpython-312.pyc 	modified:   src/bso.py 	modified:   src/cartas.py

--- src/bso.py
+++ src/bso.py
@@ -11,7 +11,7 @@ def crear_dicc_titulos_anyos(bsos:List[Tuple[str,int]])->Dict[str, int]:
     :return: Un diccionario que tiene como clave los títulos y como valores los años
     :rtype: {str:int}
     '''
-    pass
+    return dict(bsos)
 
 def crear_dicc_titulos_anyos2(bsos:List[Tuple[str,int]])->Dict[str, int]:
     '''
@@ -22,8 +22,9 @@ def crear_dicc_titulos_anyos2(bsos:List[Tuple[str,int]])->Dict[str, int]:
     :return: Un diccionario que tiene como clave los títulos y como valores los años
     :rtype: {str:int}
     '''
-    pass
+    return {x:y for x,y in bsos}
 
+from collections import Counter, defaultdict
 def crear_dicc_anyos_conteo_titulos (bsos:List[Tuple[str,int]])->Dict[int, int]:
     '''
     Recibe:
@@ -34,7 +35,7 @@ def crear_dicc_anyos_conteo_titulos (bsos:List[Tuple[str,int]])->Dict[int, int]:
           de ese año
     :rtype: {int:int}
     '''
-    pass
+    return Counter(anyo for _, anyo in bsos)
 
 def crear_dicc_anyos_lista_titulos (bsos:List[Tuple[str,int]])->Dict[int, List[str]]:
     '''
@@ -46,7 +47,10 @@ def crear_dicc_anyos_lista_titulos (bsos:List[Tuple[str,int]])->Dict[int, List[s
           de ese año
     :rtype:{int:[str]}
     '''
-    pass
+    lista_titulos_por_anyo = defaultdict(list)
+    for titulo,anyo in bsos:
+        lista_titulos_por_anyo[anyo].append(titulo)
+    return lista_titulos_por_anyo
 
 def obtener_clave_mayor(dicc_bso:Dict[str,int])->str:
     '''
@@ -57,7 +61,7 @@ def obtener_clave_mayor(dicc_bso:Dict[str,int])->str:
     :return: La clave con valor mayor, segun el orden natural
     :rtype: str
     '''
-    pass
+    return max(dicc_bso.keys())
 
 def obtener_valor_mayor(dicc_bso:Dict[str,int])->int:
     '''
@@ -68,7 +72,7 @@ def obtener_valor_mayor(dicc_bso:Dict[str,int])->int:
     :return: El valor mayor, que en este caso es el año más reciente.
     :rtype: int
     '''
-    pass
+    return max(dicc_bso.values())
 
 def obtener_nombre_mas_largo(dicc_bso:Dict[str,int])->str:
     '''
@@ -79,4 +83,4 @@ def obtener_nombre_mas_largo(dicc_bso:Dict[str,int])->str:
     :return: El nombre de la canción con más caracteres
     :rtype: str
     '''
-    pass
+    return max(dicc_bso.keys(), key=lambda n: len(n))
--- src/cartas.py
+++ src/cartas.py
@@ -6,6 +6,7 @@ from typing import NamedTuple, List, Dict
 
 Carta = NamedTuple("Carta", [("palo",str),("valor",int)])
 
+from collections import Counter, defaultdict
 def crear_dicc_conteo_valores (cartas:List[Carta])->Dict[int, int]:
     '''
     Recibe:
@@ -15,7 +16,7 @@ def crear_dicc_conteo_valores (cartas:List[Carta])->Dict[int, int]:
     :return: Un diccionario que tiene como clave los valores de las cartas y como valores el número de cartas (frecuencia) con ese valor
     :rtype: {int:int}
     '''
-    pass
+    return Counter(x. valor for x in cartas)
 
 def crear_dicc_valores_por_palos (cartas:List[Carta])->Dict[str, List[int]]:
     '''
@@ -26,7 +27,10 @@ def crear_dicc_valores_por_palos (cartas:List[Carta])->Dict[str, List[int]]:
     :return: Un diccionario que tiene como clave los palos y como valores una lista con los valores de las cartas de ese palo
     :rtype: {str:[int]}
     '''
-    pass
+    lista_valores_por_palos = defaultdict(list)
+    for x in cartas:
+        lista_valores_por_palos[x.palo].append(x.valor)
+    return lista_valores_por_palos
 
 
 def obtener_clave_mayor(dicc:Dict[int, int])->int:
@@ -38,7 +42,7 @@ def obtener_clave_mayor(dicc:Dict[int, int])->int:
     :return: La clave con valor mayor, según el orden natural
     :rtype: int
     '''
-    pass
+    return max(dicc.keys())
 
 def obtener_valor_mayor_frecuencia(dicc:Dict[int, int])->int:
     '''
@@ -49,8 +53,9 @@ def obtener_valor_mayor_frecuencia(dicc:Dict[int, int])->int:
     :return: El valor mayor, que en ese caso es el valor de carta con una frecuencia mayor
     :rtype: int
     '''
-    pass
+    return max(dicc.items(), key=lambda n: n[1])[0]
 
+from statistics import mean
 def obtener_media_valores_por_palos(cartas:List[Carta])->Dict[str, float]:
     '''
     Recibe:
@@ -60,7 +65,7 @@ def obtener_media_valores_por_palos(cartas:List[Carta])->Dict[str, float]:
     :return: Un diccionario en el que la clave son los palos y los valores, la media de los valores de las cartas de ese palo
     :rtype: {str:float} 
     '''
-    pass
+    return {palo:mean(valores) for palo, valores in crear_dicc_valores_por_palos(cartas).items()}
 
 def obtener_max_valores_por_palos(cartas:List[Carta])->Dict[str, int]:
     '''
@@ -71,4 +76,4 @@ def obtener_max_valores_por_palos(cartas:List[Carta])->Dict[str, int]:
     :return: Un diccionario en el que la clave son los palos y los valores, el mayor valor de una carta de ese palo
     :rtype: {str: int}
     '''
-    pass
+    return {palo:max(valores) for palo, valores in crear_dicc_valores_por_palos(cartas).items()}
